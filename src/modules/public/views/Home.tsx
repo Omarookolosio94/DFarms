6c83cd7bb67f1bd06c1bfc5fc26ba76d777c51c5
Oghenemaro Okolosio <omarookolosio94@gmail.com>
2024-04-28 12:17:22 +0100

    updated meta data for index

--- src/modules/public/views/Home.tsx
+++ src/modules/public/views/Home.tsx
@@ -23,7 +23,7 @@ const Home = () => {
         title: "Deka Farms - Home",
         path: "home",
         description:
-          " Welcome to Deka Farms, where agriculture meets innovation! At Deka Farms, we pride ourselves on our commitment to sustainable farming practices and providing top- quality products to our customers. From our fertile fields to your table, we strive for excellence in every aspect of our operation  ",
+          "Welcome to Deka Farms, where agriculture meets innovation! At Deka Farms, we pride ourselves on our commitment to sustainable farming practices and providing top- quality products to our customers. From our fertile fields to your table, we strive for excellence in every aspect of our operation  ",
       })}
 
       <section className="bg-home h-[100vh]">
